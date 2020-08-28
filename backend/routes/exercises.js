90c260ef76dacd3efb75f2cbeb3170f7a1dbe6c6
seedy1 <jobe.seedy@gmail.com>
2020-08-28 01:07:19 +0000

    added edit and delete end points for the exercise model

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -23,4 +23,32 @@ router.route("/add").post( (req, res) => {
 
  } );
 
+router.route("/:id").get( (req, res) =>{
+     Exercise.findById(req.params.id).then(ex => res.json(ex))
+                            .catch(err => res.status(400).json("Error: "+err));
+ } );
+
+ router.route("/:id").delete( (req, res) =>{
+    Exercise.findByIdAndDelete(req.params.id).then(() => res.json("Deleted!"))
+                           .catch(err => res.status(400).json("Error: "+err));
+} );
+
+
+router.route("/update/:id").post( (req, res) =>{
+    Exercise.findById(req.params.id)
+    .then(ex =>{
+        ex.username = req.body.username;
+        ex.description = req.body.description;
+        ex.duration = Number(req.body.duration);
+        ex.date =  Date.parse(req.body.date );
+
+        ex.save().then( ()=> res.json("Exercise updated") )
+        .catch(err => res.status(400).json("Error: "+err));;
+
+    } )
+    .catch(err => res.status(400).json("Error: "+err));
+} );
+
+
+
  module.exports = router;
